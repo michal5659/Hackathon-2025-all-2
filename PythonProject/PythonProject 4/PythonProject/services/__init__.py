f09784a3404a28006785884eeffce12854a4e9fb
michal5659 <70981937+michal5659@users.noreply.github.com>
2025-11-06 22:53:38 +0200

    Delete PythonProject directory

--- PythonProject/PythonProject 4/PythonProject/services/__init__.py
+++
@@ -1,8 +0,0 @@
-# from .classification_agent import get_classification_agent, ClassificationAgent
-# from .task_creation_agent import get_task_creation_agent, TaskCreationAgent
-from PythonProject.services.api_utils import get_api_utils, ApiUtils
-
-__all__ = [
-    "get_api_utils",
-    "ApiUtils",
-]
